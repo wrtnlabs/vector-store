32b2d20b60ad0590efc54ee2aa0d81fab190aafc
kakasoo <kscodebase@gmail.com>
2025-02-12 15:51:15 +0900

    fix: fix property name and meaning of `IPagination`

--- src/types/IPagination.ts
+++ src/types/IPagination.ts
@@ -1,6 +1,14 @@
 import { tags } from "typia";
 
 export interface IPagination {
-  limit: number & tags.Type<"int64">;
-  offset: number & tags.Type<"int64">;
+  /**
+   * This refers to the number of elements per page.
+   */
+  count: number & tags.Type<"int64">;
+
+  /**
+   * It refers to the page on which data is to be searched.
+   * The method of calculation is bound to be affected by the count.
+   */
+  page: number & tags.Type<"int64">;
 }
