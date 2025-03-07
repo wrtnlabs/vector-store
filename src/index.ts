de03c088f1bd1a20663088fd18fb5fb4fd0578e3
kakasoo <kscodebase@gmail.com>
2025-03-07 16:12:33 +0900

    feat: export types and publish

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,4 @@
+export * from "./types/index";
+
 export * from "./file";
 export * from "./OpenAIVectorStore";
---
+++ src/types/index.ts
@@ -0,0 +1,6 @@
+export * from "./IFile";
+export * from "./IPagination";
+export * from "./IProvider";
+export * from "./IStore";
+export * from "./IVectorStore";
+export * from "./IVectorStoreFile";
